a84727b1bfc32473b44229fb7b506b24c1d67de2
ChrisShen <857975430@qq.com>
2023-04-27 19:44:40 +0800

    fix windows

--- convert.py
+++ convert.py
@@ -3,28 +3,41 @@ from pydub import AudioSegment
 from pydub.silence import detect_silence
 import zipfile
 
-
 input_dir = input("请输入要导入的文件夹位置（回车确认）：")
-input_dir = os.path.normpath(input_dir)
-print(input_dir)
+# input_dir = os.path.normpath(input_dir)
+
+if "\\" in input_dir:
+    input_dir = input_dir.strip('"')
+else:
+    input_dir += "/"
+
+#print(input_dir)
 output_dir = input("请输入要导出的文件夹位置（回车确认）：")
-output_dir = os.path.normpath(output_dir)
-print(output_dir)
+# output_dir = os.path.normpath(output_dir)
+
+if "\\" in output_dir:
+    output_dir = output_dir.strip('"')
+else:
+    output_dir += "/"
+
+#print(output_dir)
 is_zip = int(input("是否需要压缩（1需要，0不需要，回车确认）："))
 if is_zip:
     zip_name = input("请输入要导出的压缩包名（直接回车则默认使用导入的文件夹名称）：")
     if not zip_name:
         zip_name = os.path.basename(input_dir)
 
-input_dir += "/"
-output_dir += "/"
+# input_dir += "/"
+# output_dir += "/"
 # 定义可处理的文件格式列表
 ALLOWED_EXTENSIONS = {'wav', 'm4a', 'aac', 'mp3'}
+
 
 # 检查文件是否在可处理的文件格式列表中
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 for file_name in os.listdir(input_dir):
     if not allowed_file(file_name):
@@ -43,19 +56,19 @@ for file_name in os.listdir(input_dir):
     print(f"音频长度：{len(audio)} ms")
     print(f"平均音量：{audio.dBFS} dBFS")
 
-    #for i, (start, end) in enumerate(silent_ranges):
-        #print(f"静音段{i+1}: {start} ms - {end} ms")
+    # for i, (start, end) in enumerate(silent_ranges):
+    # print(f"静音段{i+1}: {start} ms - {end} ms")
 
     if silent_ranges:
-        start_trim = silent_ranges[0][1] # 起始时间为第一个静音段的结束时间
-        end_trim = silent_ranges[-1][0] # 结束时间为最后一个静音段的起始时间
-        audio = audio[start_trim:end_trim] # 裁剪音频文件
+        start_trim = silent_ranges[0][1]  # 起始时间为第一个静音段的结束时间
+        end_trim = silent_ranges[-1][0]  # 结束时间为最后一个静音段的起始时间
+        audio = audio[start_trim:end_trim]  # 裁剪音频文件
 
     # 导出音频文件
     output_path = os.path.join(output_dir, os.path.splitext(file_name)[0] + '.wav')
     audio.export(output_path, format="wav", bitrate="128k", parameters=["-ar", "8000"])
     print(f"导出文件：{output_path}")
-    #audio.export(output_path, format="wav")
+    # audio.export(output_path, format="wav")
 
 if is_zip:
     zip_name += '.zip'
